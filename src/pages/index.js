2f2ff136bca4964455065b0f5c8b035d0a4bde1d
Jared Brown <jared.brown@elementthree.com>
2020-04-16 13:24:36 -0400

    Add modal form

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,39 +4,112 @@ import SEO from "../components/seo"
 import { motion } from "framer-motion"
 // import "../styles/main.scss"
 
-// const Modal = (props) => {
-//   return (
-//     <div className="fixed inset-0 bg-gray-900 flex justify-center items-center z-30 px-4">
-//       <div className="bg-white rounded-md p-12 shadow-md mx-auto max-w-lg flex-grow">
-//         <div className="text-gray-700 leading-tight text-sm">
-//           Estimated Monthly Payment
-//           <br />
-//           <strong className="inline-block text-5xl">
-//             {" "}
-//             <span className="inline-block font-normal text-2xl transform -translate-y-3">
-//               $
-//             </span>
-//             {props.monthlyPayment}
-//           </strong>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
+const Modal = props => {
+  const [fName, setFName] = React.useState("")
+  const [lName, setLName] = React.useState("")
+  const [phone, setPhone] = React.useState("")
+  return (
+    <div className="fixed inset-0 bg-gray-900 flex justify-center items-center z-30 px-4">
+      <div className="bg-white rounded-md p-12 shadow-md mx-auto max-w-lg flex-grow">
+        <h2>Your estimated mortgage</h2>
+        <div className="text-gray-700 leading-tight text-sm mt-8">
+          Mortgage Amount
+          <br />
+          <strong className="inline-block text-5xl">
+            {" "}
+            <span className="inline-block font-normal text-2xl transform -translate-y-3">
+              $
+            </span>
+            {props.principal}
+          </strong>
+        </div>
+        <div className="text-gray-700 leading-tight text-sm">
+          Interest Rate
+          <br />
+          <strong className="inline-block text-5xl"> {props.interest}</strong>
+          <span className="inline-block font-normal text-2xl transform -translate-y-3">
+            %
+          </span>
+        </div>
+        <div className="text-gray-700 leading-tight text-sm">
+          Loan Term
+          <br />
+          <strong className="inline-block text-5xl">{props.loanTerm} </strong>
+          <span className="inline-block font-normal text-2xl transform -translate-y-3">
+            {" "}
+            years
+          </span>
+        </div>
+        <div className="text-gray-700 leading-tight text-sm">
+          Estimated Monthly Payment
+          <br />
+          <strong className="inline-block text-5xl">
+            {" "}
+            <span className="inline-block font-normal text-2xl transform -translate-y-3">
+              $
+            </span>
+            {props.monthlyPayment}
+          </strong>
+        </div>
+        <form action="">
+          <label className="block mt-4">
+            <span className="text-gray-700 text-sm">First Name</span>
+            <input
+              type="text"
+              className="form-input mt-1 block w-full"
+              placeholder="John"
+              value={fName}
+              onChange={e => setFName(e.target.value)}
+            />
+          </label>
+          <label className="block mt-4">
+            <span className="text-gray-700 text-sm">Last Name</span>
+            <input
+              type="text"
+              className="form-input mt-1 block w-full"
+              placeholder="Smith"
+              value={lName}
+              onChange={e => setLName(e.target.value)}
+            />
+          </label>
+          <label className="block mt-4">
+            <span className="text-gray-700 text-sm">Phone Number</span>
+            <input
+              type="phone"
+              className="form-input mt-1 block w-full"
+              placeholder="(555) 555-5555"
+              value={phone}
+              onChange={e => setPhone(e.target.value)}
+            />
+          </label>
+          <button className="bg-blue-700 text-white font-bold px-5 py-3 rounded mt-8">
+            Get Started
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
 
 // todo: convert to useReducer
 const IndexPage = () => {
   let monthlyPayment //monthly mortgage payment
-  const [principle, setPrinciple] = React.useState(225000)
+  const [principal, setPrincipal] = React.useState(225000)
   const [interest, setInterest] = React.useState(3.86)
   const [loanTerm, setLoanTerm] = React.useState(30)
   const [clickCount, setClickCount] = React.useState(0)
+  const [isFormOpen, setIsFormOpen] = React.useState(false)
 
   const moneyFormatter = new Intl.NumberFormat("en-US", {
     // style: "currency",
     // currency: "USD",
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
+  })
+
+  const percentFormatter = new Intl.NumberFormat("en-US", {
+    minimumFractionDigits: 3,
+    maximumFractionDigits: 3,
   })
 
   function calculateMonthlyPayment(p, n, i) {
@@ -47,7 +120,7 @@ const IndexPage = () => {
 
   //set monthly mortgage payment
   monthlyPayment = calculateMonthlyPayment(
-    principle,
+    principal,
     loanTerm * 12,
     interest / 100 / 12
   )
@@ -55,7 +128,14 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
-      {/* <Modal monthlyPayment={monthlyPayment}/> */}
+      {isFormOpen && (
+        <Modal
+          monthlyPayment={monthlyPayment}
+          interest={interest}
+          principal={principal}
+          loanTerm={loanTerm}
+        />
+      )}
       <section className="bg-gray-300 h-screen w-full pt-24">
         <div className="container h-full flex items-start justify-center">
           {/* Start Calculator with inputs */}
@@ -76,24 +156,42 @@ const IndexPage = () => {
             </div>
             <form action="" className="mt-5">
               <label className="block">
-                <span className="text-gray-700 text-sm">Mortgage Amount</span>
-                <input
+                <span className="text-gray-700 text-sm">
+                  Mortgage Amount $
+                  <strong>{moneyFormatter.format(principal)}</strong>
+                </span>
+                {/* <input
                   className="form-input mt-1 block w-full"
                   placeholder="$225,000"
                   type="number"
                   min="25000"
                   max="450000"
-                  value={principle}
+                  value={principal}
                   onChange={e => setPrinciple(e.target.value)}
+                  onBlur={() => setClickCount(clickCount => clickCount + 1)}
+                /> */}
+                <input
+                  className="form-input mt-1 block w-full"
+                  placeholder="$225,000"
+                  type="range"
+                  min="25000"
+                  max="450000"
+                  step="1000"
+                  value={principal}
+                  onChange={e => setPrincipal(e.target.value)}
                   onBlur={() => setClickCount(clickCount => clickCount + 1)}
                 />
               </label>
               <label className="block mt-4">
-                <span className="text-gray-700 text-sm">Interest Rate</span>
+                <span className="text-gray-700 text-sm">
+                  Interest Rate{" "}
+                  <strong>{percentFormatter.format(interest)}</strong>%
+                </span>
                 <input
-                  type="integer"
+                  type="range"
                   min="2"
                   max="10"
+                  step=".125"
                   className="form-input mt-1 block w-full"
                   placeholder="4.6%"
                   value={interest}
@@ -123,9 +221,9 @@ const IndexPage = () => {
                   animate={{ y: 0, opacity: 1 }}
                   // transition={{ ease: "easeIn", duration: 0.25 }}
                 >
-                  <button className="bg-teal-700 text-white font-bold px-5 py-3 rounded">
+                  <div className="inline-block bg-teal-700 text-white font-bold px-5 py-3 rounded" onClick={() => setIsFormOpen(true)}>
                     Get Started
-                  </button>
+                  </div>
                 </motion.div>
               ) : null}
               <p className="mx-auto max-w-lg flex-grow mt-8 text-xs text-gray-600 italic">
