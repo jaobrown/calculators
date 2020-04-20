e396f03fb08b7ae0fc0b8ea3683d0591eb397f63
Jared Brown <jared.brown@elementthree.com>
2020-04-20 13:43:45 -0400

    Implement UI element styles for calculator demo

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,98 +16,98 @@ const percentFormatter = new Intl.NumberFormat("en-US", {
   maximumFractionDigits: 3,
 })
 
-const Modal = props => {
-  const [fName, setFName] = useState("")
-  const [lName, setLName] = useState("")
-  const [phone, setPhone] = useState("")
-  return (
-    <div className="fixed inset-0 overflow-y-scroll bg-gray-900 z-30 px-4">
-      <div className="my-8 bg-white rounded-md p-12 shadow-md mx-auto max-w-lg flex-grow">
-        <h2>Your estimated mortgage</h2>
-        <div className="text-gray-700 leading-tight text-sm mt-8">
-          Mortgage Amount
-          <br />
-          <strong className="inline-block text-5xl">
-            {" "}
-            <span className="inline-block font-normal text-2xl transform -translate-y-3">
-              $
-            </span>
-            {moneyFormatter.format(props.principal)}
-          </strong>
-        </div>
-        <div className="text-gray-700 leading-tight text-sm">
-          Interest Rate
-          <br />
-          <strong className="inline-block text-5xl"> {props.interest}</strong>
-          <span className="inline-block font-normal text-2xl transform -translate-y-3 translate-x-1">
-            %
-          </span>
-        </div>
-        <div className="text-gray-700 leading-tight text-sm">
-          Loan Term
-          <br />
-          <strong className="inline-block text-5xl">{props.loanTerm} </strong>
-          <span className="inline-block font-normal text-2xl transform -translate-y-3 translate-x-1">
-            years
-          </span>
-        </div>
-        <div className="text-gray-700 leading-tight text-sm">
-          Estimated Monthly Payment
-          <br />
-          <strong className="inline-block text-5xl">
-            {" "}
-            <span className="inline-block font-normal text-2xl transform -translate-y-3">
-              $
-            </span>
-            {props.monthlyPayment}
-          </strong>
-        </div>
-        <form action="">
-          <label className="block mt-4">
-            <span className="text-gray-700 text-sm">First Name</span>
-            <input
-              type="text"
-              className="form-input mt-1 block w-full"
-              placeholder="John"
-              value={fName}
-              onChange={e => setFName(e.target.value)}
-            />
-          </label>
-          <label className="block mt-4">
-            <span className="text-gray-700 text-sm">Last Name</span>
-            <input
-              type="text"
-              className="form-input mt-1 block w-full"
-              placeholder="Smith"
-              value={lName}
-              onChange={e => setLName(e.target.value)}
-            />
-          </label>
-          <label className="block mt-4">
-            <span className="text-gray-700 text-sm">Phone Number</span>
-            <input
-              type="phone"
-              className="form-input mt-1 block w-full"
-              placeholder="(555) 555-5555"
-              value={phone}
-              onChange={e => setPhone(e.target.value)}
-            />
-          </label>
-          <button className="bg-blue-700 text-white font-bold px-5 py-3 rounded mt-8">
-            Get Started
-          </button>
-        </form>
-      </div>
-    </div>
-  )
-}
+// const Modal = props => {
+//   const [fName, setFName] = useState("")
+//   const [lName, setLName] = useState("")
+//   const [phone, setPhone] = useState("")
+//   return (
+//     <div className="fixed inset-0 overflow-y-scroll bg-gray-900 z-30 px-4">
+//       <div className="my-8 bg-white rounded-md p-12 shadow-md mx-auto max-w-lg flex-grow">
+//         <h2>Your estimated mortgage</h2>
+//         <div className="text-gray-700 leading-tight text-sm mt-8">
+//           Mortgage Amount
+//           <br />
+//           <strong className="inline-block text-5xl">
+//             {" "}
+//             <span className="inline-block font-normal text-2xl transform -translate-y-3">
+//               $
+//             </span>
+//             {moneyFormatter.format(props.principal)}
+//           </strong>
+//         </div>
+//         <div className="text-gray-700 leading-tight text-sm">
+//           Interest Rate
+//           <br />
+//           <strong className="inline-block text-5xl"> {props.interest}</strong>
+//           <span className="inline-block font-normal text-2xl transform -translate-y-3 translate-x-1">
+//             %
+//           </span>
+//         </div>
+//         <div className="text-gray-700 leading-tight text-sm">
+//           Loan Term
+//           <br />
+//           <strong className="inline-block text-5xl">{props.loanTerm} </strong>
+//           <span className="inline-block font-normal text-2xl transform -translate-y-3 translate-x-1">
+//             years
+//           </span>
+//         </div>
+//         <div className="text-gray-700 leading-tight text-sm">
+//           Estimated Monthly Payment
+//           <br />
+//           <strong className="inline-block text-5xl">
+//             {" "}
+//             <span className="inline-block font-normal text-2xl transform -translate-y-3">
+//               $
+//             </span>
+//             {props.monthlyPayment}
+//           </strong>
+//         </div>
+//         <form action="">
+//           <label className="block mt-4">
+//             <span className="text-gray-700 text-sm">First Name</span>
+//             <input
+//               type="text"
+//               className="form-input mt-1 block w-full"
+//               placeholder="John"
+//               value={fName}
+//               onChange={e => setFName(e.target.value)}
+//             />
+//           </label>
+//           <label className="block mt-4">
+//             <span className="text-gray-700 text-sm">Last Name</span>
+//             <input
+//               type="text"
+//               className="form-input mt-1 block w-full"
+//               placeholder="Smith"
+//               value={lName}
+//               onChange={e => setLName(e.target.value)}
+//             />
+//           </label>
+//           <label className="block mt-4">
+//             <span className="text-gray-700 text-sm">Phone Number</span>
+//             <input
+//               type="phone"
+//               className="form-input mt-1 block w-full"
+//               placeholder="(555) 555-5555"
+//               value={phone}
+//               onChange={e => setPhone(e.target.value)}
+//             />
+//           </label>
+//           <button className="bg-blue-700 text-white font-bold px-5 py-3 rounded mt-8">
+//             Get Started
+//           </button>
+//         </form>
+//       </div>
+//     </div>
+//   )
+// }
 
 // todo: convert to useReducer
 const IndexPage = () => {
   let monthlyPayment //monthly mortgage payment
   const [principal, setPrincipal] = useState(225000)
   const [interest, setInterest] = useState(3.86)
-  const [loanTerm, setLoanTerm] = useState(30)
+  const [loanTerm, setLoanTerm] = useState("30 years")
   const [clickCount, setClickCount] = useState(0)
   const [isFormOpen, setIsFormOpen] = useState(false)
 
@@ -117,37 +117,41 @@ const IndexPage = () => {
     )
   }
 
+  // console.log(loanTerm)
+
+  let strippedLoanTerm = +loanTerm.split(" ")[0]
+
   //set monthly mortgage payment
   monthlyPayment = calculateMonthlyPayment(
     principal,
-    loanTerm * 12,
+    strippedLoanTerm * 12,
     interest / 100 / 12
   )
 
   return (
     <Layout>
       <SEO title="Home" />
-      {isFormOpen && (
+      {/* {isFormOpen && (
         <Modal
           monthlyPayment={monthlyPayment}
           interest={interest}
           principal={principal}
           loanTerm={loanTerm}
         />
-      )}
+      )} */}
       <section className="bg-gray-300 min-h-screen w-full py-24">
         <div className="container h-full flex items-start justify-center">
           {/* Start Calculator with inputs */}
-          <div className="bg-white rounded-md p-12 shadow-md mx-auto max-w-lg flex-grow">
-            <h1 className="font-semibold text-3xl text-gray-800 leading-tight">
-              New Home Mortgage Payment Estimator
-            </h1>
-            <div className="mt-8 text-gray-700 leading-tight text-sm">
+          <div className="bg-white p-12 shadow-md mx-auto max-w-lg flex-grow">
+            <h2 className="font-medium text-3xl text-brand-royal-blue leading-tight">
+              Mortgage Calculator
+            </h2>
+            <div className="mt-8 text-brand-gray-300 leading-tight text-sm">
               Estimated Monthly Payment
               <br />
-              <strong className="inline-block text-5xl">
+              <strong className="font-medium inline-block text-6xl text-brand-teal">
                 {" "}
-                <span className="inline-block font-normal text-2xl transform -translate-y-3">
+                <span className="inline-block font-normal text-2xl transform -translate-y-4">
                   $
                 </span>
                 {monthlyPayment}
@@ -155,12 +159,18 @@ const IndexPage = () => {
             </div>
             <form action="" className="mt-5">
               <label className="block">
-                <span className="text-gray-700 text-sm">
-                  Mortgage Amount $
-                  <strong>{moneyFormatter.format(principal)}</strong>
+                <span className="text-brand-gray-300 text-sm leading-tight">
+                  Home Price <br />
+                  <strong className="font-medium inline-block text-4xl text-brand-royal-blue">
+                    {" "}
+                    <span className="inline-block font-normal text-lg transform -translate-y-2">
+                      $
+                    </span>
+                    {moneyFormatter.format(principal)}
+                  </strong>
                 </span>
                 <input
-                  className="form-input mt-1 block w-full"
+                  className="form-input mt-2 block w-full border-none p-0"
                   placeholder="$225,000"
                   type="range"
                   min="25000"
@@ -172,16 +182,21 @@ const IndexPage = () => {
                 />
               </label>
               <label className="block mt-4">
-                <span className="text-gray-700 text-sm">
-                  Interest Rate{" "}
-                  <strong>{percentFormatter.format(interest)}</strong>%
+                <span className="text-brand-gray-300 text-sm leading-tight">
+                  Interest Rate <br />
+                  <strong className="font-medium inline-block text-4xl text-brand-royal-blue">
+                    {percentFormatter.format(interest)}
+                    <span className="inline-block font-normal text-lg transform -translate-y-2 translate-x-1">
+                      %
+                    </span>
+                  </strong>
                 </span>
                 <input
                   type="range"
                   min="3"
                   max="6.5"
                   step=".125"
-                  className="form-input mt-1 block w-full"
+                  className="form-input mt-2 block w-full border-none p-0"
                   placeholder="4.6%"
                   value={interest}
                   onChange={e => setInterest(e.target.value)}
@@ -189,29 +204,31 @@ const IndexPage = () => {
                 />
               </label>
               <label className="block mt-4">
-                <span className="text-gray-700 text-sm">Loan Duration</span>
+                <span className="text-brand-gray-300 text-sm">
+                  Loan Duration
+                </span>
                 <select
-                  className="form-select mt-1 block w-full"
+                  className="form-select mt-2 block w-1/2 rounded-none border-4 border-brand-royal-blue text-brand-royal-blue font-semibold"
                   value={loanTerm}
                   onChange={e => setLoanTerm(e.target.value)}
                   onClick={() => setClickCount(clickCount => clickCount + 1)}
                 >
-                  <option>10</option>
-                  <option>15</option>
-                  <option>20</option>
-                  <option>25</option>
-                  <option>30</option>
+                  <option>10 years</option>
+                  <option>15 years</option>
+                  <option>20 years</option>
+                  <option>25 years</option>
+                  <option>30 years</option>
                 </select>
               </label>
               {clickCount >= 2 ? (
                 <motion.div
-                  className="mt-4"
+                  className=" mt-10"
                   initial={{ y: 25, opacity: 0 }}
                   animate={{ y: 0, opacity: 1 }}
                   // transition={{ ease: "easeIn", duration: 0.25 }}
                 >
                   <button
-                    className="inline-block bg-teal-700 text-white font-bold px-5 py-3 rounded"
+                    className="inline-block bg-brand-teal text-white font-normal px-10 py-3"
                     onClick={() => setIsFormOpen(true)}
                     type="button"
                   >
@@ -219,7 +236,7 @@ const IndexPage = () => {
                   </button>
                 </motion.div>
               ) : null}
-              <p className="mx-auto max-w-lg flex-grow mt-8 text-xs text-gray-600 italic">
+              <p className="mx-auto max-w-lg flex-grow mt-10 text-xs text-gray-600 italic">
                 Payments reflect principal and interest only and do not include
                 taxes and insurance. Actual payment obligation will be higher.
               </p>
@@ -227,7 +244,7 @@ const IndexPage = () => {
                 href="https://royalunitedmortgage.com/disclosures/"
                 rel="noopener noreferrer"
                 target="_blank"
-                className="underline block mx-auto max-w-lg flex-grow mt-2 text-xs text-gray-700"
+                className="underline block mx-auto max-w-lg flex-grow mt-2 text-xs text-brand-gray-300"
               >
                 Disclosures
               </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
 module.exports = {
   theme: {
     fontFamily: {
-      'sans': ['Work Sans', 'sans-serif'],
-      'body': ['Work Sans', 'sans-serif'],
+      sans: ["Work Sans", "sans-serif"],
+      body: ["Work Sans", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        "brand-gray-300": "#171819",
+        "brand-gray-200": "#4A5568",
+        "brand-gray-100": "#A0A0A0",
+        "brand-royal-blue": "#111C4E",
+        "brand-teal": "#00A19C",
+      },
+    },
     container: {
       center: true,
     },
   },
   variants: {},
-  plugins: [require("@tailwindcss/custom-forms")]
+  plugins: [require("@tailwindcss/custom-forms")],
 }
